b65bbc0a195635f9eaf97dedb8b3a0fdb115dc6a
yyaroshevich <yyaroshevich@exadel.com>
2019-01-03 14:31:09 +0300

    task1 - examples and description

--- task1 (sum)/task1.js
+++ task1 (sum)/task1.js
@@ -1,3 +1,10 @@
+/*
+* Problem:
+* Create a function sum with a numeric parameter.
+* The output of console.log(sum(1)(2)...(k)) should be a number (1 + 2 + ... + k)
+* */
+
+/* Solution */
 function sum(num) {
 	let acc = num;
 
@@ -13,4 +20,6 @@ function sum(num) {
 	return newSum;
 }
 
+/* Testing */
 console.log(sum(1)(2)(3));
+console.log(sum(3)(3)(3));
